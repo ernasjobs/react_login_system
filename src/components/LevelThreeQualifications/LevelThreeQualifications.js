181a5893512cd19e8de3933065c9d3059303e7f0
enasjobs <ernasjobs@gmail.com>
2020-11-23 22:18:03 +0000

    L3QA

--- src/components/LevelThreeQualifications/LevelThreeQualifications.js
+++ src/components/LevelThreeQualifications/LevelThreeQualifications.js
@@ -28,7 +28,7 @@ const LevelThreeQualifications = () => {
     
     <div className={className2}>
       <div className="form-background">
-        <h5 className="form-title">What Level 3 qualifications does the applicant hold?</h5>
+        <h3 className="form-title">Level 3 Qualifications?</h3>
         {counter < 6 ?
           <div className={className1}>
             <L3Component id={counter} clearInput= {clearInput}  />
